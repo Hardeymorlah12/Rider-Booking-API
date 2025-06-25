1d8d6c77527ecea17aa070ad0195fc762dc5a7c9
Hardeymorlah <Hardeymorlah12@gmail.com>
2025-06-26 00:08:46 +0100

    Initial commit: Rider Booking API with Spring Boot

---
+++ src/main/java/com/tech1971/rider_booking_api/RiderBookinApiApplication.java
@@ -0,0 +1,13 @@
+package com.tech1971.rider_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderBookinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderBookinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech1971/rider_booking_api/RiderBookinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech1971.rider_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderBookinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
